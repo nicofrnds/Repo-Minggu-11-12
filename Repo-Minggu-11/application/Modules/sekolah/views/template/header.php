5e2dbb24a5de63b543a9e096e2ea756844ae61ff
Nico F <62508863+nicofrnds@users.noreply.github.com>
2020-06-08 19:23:08 +0700

    Update_header

--- Repo-Minggu-11/application/Modules/sekolah/views/template/header.php
+++ Repo-Minggu-11/application/Modules/sekolah/views/template/header.php
@@ -16,7 +16,7 @@
             <br>
 
             <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-                <a class="navbar-brand" href="<?= base_url('sekolah') ?>">CRUD - Codeigniter ( 02734 )</a>
+                <a class="navbar-brand" href="<?= base_url('sekolah/awal') ?>">CRUD - Codeigniter ( 02734 )</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -25,7 +25,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="<?= base_url('sekolah') ?>">Home <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="<?= base_url('sekolah/awal') ?>">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="<?= base_url('sekolah/awal/sekolah') ?>">Sekolah</a>
