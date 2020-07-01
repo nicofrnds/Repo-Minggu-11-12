13d69f41df57142dcbc8e50c733e52f555840f6e
nicofrnds <nico231737@gmail.com>
2020-07-02 00:00:18 +0700

    Upload Praktikum 11

---
+++ tokobuah/application/controllers/admin/Overview.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Overview extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		// load view admin/overview.php
+		$this->load->view("admin/overview");
+	}
+}
